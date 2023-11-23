cb5f1a84914f435dda8c9956bf3077ba698831f4
IAmAStealer <alexisstealer@gmail.com>
2023-11-23 21:15:42 +0100

    Update constantes.py for python compat

--- BraceletGenerator/constantes.py
+++ BraceletGenerator/constantes.py
@@ -131,7 +131,10 @@ if LANGUE not in ["en", "fr"]:
     CONFIG.set("General", "language", LANGUE[:2])
 
 gettext.find(APP_NAME, PATH_LOCALE)
-gettext.bind_textdomain_codeset(APP_NAME, "UTF-8")
+try:
+    gettext.bind_textdomain_codeset(APP_NAME, 'UTF-8')
+except AttributeError:
+    pass
 gettext.bindtextdomain(APP_NAME, PATH_LOCALE)
 gettext.textdomain(APP_NAME)
 LANG = gettext.translation(APP_NAME, PATH_LOCALE,
